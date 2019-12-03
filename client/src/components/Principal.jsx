f2cda3244d02e94c474adf4cd097303fa6960b8a
Matthew Beckerleg <ord.sc2@gmail.com>
2019-12-02 16:45:13 -0800

    first attempt at styling page elements

--- client/src/components/Principal.jsx
+++ client/src/components/Principal.jsx
@@ -4,22 +4,47 @@ import styled from 'styled-components';
 const Principal = (props) => {
   const { price, usdF } = props;
 
-  const Head = styled.h1`
-    background: red;
-  `
+  const ParentDiv = styled.div`
+    background-color: rgba(211,211,211,0.25)
+  `;
+
+  const SectionTitle = styled.div`
+    color: rgb(42, 42, 51);
+    -webkit-font-smoothing: antialiased;
+    font-family: "Open Sans", Tahoma, Geneva, sans-serif;
+    font-weight: 700;
+    font-size: 12px;
+    line-height: 2px;
+    letter-spacing: 0px;
+    text-transform: none;
+    margin: 0px;
+    color: rgb(42, 42, 51);
+    -webkit-font-smoothing: antialiased;
+    margin: 0px;
+    box-sizing: border-box;
+  `;
+
+  const SubTitle = styled.p`
+    font-weight: 200;
+  `;
+
+  const Link = styled.a`
+    textDecoration: none;
+    font-size: 8px;
+  `;
 
   return (
-    <div>
-      <Head>
+    <ParentDiv>
+      <SectionTitle>
         Principal & Interest
-        <p>
+        <SubTitle>
           {usdF(Math.round(price / 12))}
           /mo
-        </p>
-      </Head>
-      <div>
+        </SubTitle>
+      </SectionTitle>
+      <SectionTitle>
         Home price
-        <span >
+        <span>
           <label>$</label>
           <input
             type="text"
@@ -27,21 +52,26 @@ const Principal = (props) => {
             value={usdF(price)}
           />
         </span>
-      </div>
-      <div>
+      </SectionTitle>
+      <SectionTitle>
         Down payment
         <input />
         <input />
-      </div>
-      <div>
+      </SectionTitle>
+      <SectionTitle>
         Loan program
         <input />
-      </div>
-      <div>
+      </SectionTitle>
+      <SectionTitle>
         Interest rate
         <input />
-      </div>
-    </div>
+      </SectionTitle>
+      <SectionTitle>
+          <Link href="https://www.zillow.com/mortgage-rates/?auto=true&zip=98144&value=2300000&down=460000&loantype=purchase&source=HDPModuleWOW_rates">
+            See current rates
+          </Link>
+      </SectionTitle>
+    </ParentDiv>
   );
 };
 
